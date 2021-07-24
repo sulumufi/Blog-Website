183261884b4c6a530fd5b0afda88741d4e610c59
sulu mufi <suluksm@gmail.com>
2021-07-24 12:38:53 +0530

    Mongo DB upgrade

--- app.js
+++ app.js
@@ -5,13 +5,37 @@ const bodyParser = require("body-parser");
 const _ = require('lodash');
 const ejs = require("ejs");
 
+const mongoose = require("mongoose");
+
+const url = "mongodb://localhost:27017/";
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+},function(err){
+  if(err){
+    console.log(err);
+  }else{
+    console.log("connected successfully");
+  }
+});
+
+
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
 
-const posts = [];
+const postSchema = mongoose.Schema({
+  title : String,
+  body : String
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+
+
 
 app.set('view engine', 'ejs');
 
@@ -21,10 +45,20 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", {
-    paragraph: homeStartingContent,
-    jsObject: posts
-  });
+
+  Post.find({},function(err,items_found){
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.render("home", {
+        paragraph: homeStartingContent,
+        jsObject: items_found
+      });
+    
+    }
+  })
+
 
   // console.log(posts);
 })
@@ -46,34 +80,26 @@ app.get("/compose", function (req, res) {
 })
 
 
-app.get("/posts/:testparam", function (req, res) {
+app.get("/posts/:testparam/id/:testid", function (req, res) {
   console.log(req.params.testparam);
-  console.log(posts);
+  console.log(req.params.testid);
 
   const curr_param = req.params.testparam;
+  const curr_id = req.params.testid;
 
-  // if(req.params.testparam == posts[posts.length-1].title){
-  //   console.log("match found!");
-  // }else{
-  //   console.log("Not found!")
-  // }
-
-  // if(posts.some(el => el.title === req.params.testparam)){
-  //   console.log("here it is.---------yayy!");
-  // }
-  // else{
-  //   console.log("nope!")
-  // }
-
-  posts.forEach(function(i) {
-    if (_.lowerCase(i.title) === _.lowerCase(curr_param)) {
-      console.log("-----yeyeyeye!");
+  Post.findOne({_id:curr_id}, function(err, item_found){
+    if(!err){
+      console.log(item_found);
       res.render("post", {
-        title:  i.title,
-        body: i.body
+        title:  item_found.title,
+        body: item_found.body
       })
+
     }
   })
+
+
+
 
 })
 
@@ -82,12 +108,13 @@ app.post("/compose", function (req, res) {
   postTitle = req.body.postTitle;
   postBody = req.body.postBody;
 
-  const post = {
+
+  const post1 = new Post({
     title: postTitle,
     body: postBody
-  }
+  });
 
-  posts.push(post)
+  post1.save();
 
   res.redirect("/")
 })
